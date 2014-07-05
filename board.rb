4a44721f4b14001d5d95aebe4864555dcf107eb7
Ariel Fogel & Morgan O'Leary <pair+afogel.rmhwow@devbootcamp.com>
2014-07-05 13:44:51 -0700

    initial commit

---
+++ board.rb
@@ -0,0 +1,7 @@
+class Board
+
+  def initialize(board_string)
+    @board = Array.new(9) { Array.new(9) { Array.new(9,nil) } }
+  end
+
+end
---
+++ board_spec.rb
@@ -0,0 +1,4 @@
+require 'rspec'
+RSpec.describe Board do
+
+end
